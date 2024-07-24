b772adc96670fe4c50de1b9b9bdd91e422168f3f
seongeun223 <jklun080921@gmail.com>
2024-07-24 15:10:18 +0900

    implicit

---
+++ 01_core/02_variable/03_implicit-coercion/02_convert-to-number.js
@@ -0,0 +1,38 @@
+console.log('====== 숫자 타입으로 변환 ======');
+
+// 산술 연산자의 피연산자는 모두 숫자이어야 하기 때문에
+// 숫자가 아닌 피연산자를 숫자 타입으로 암묵적 타입 변환한다.
+console.log(10 - '5');
+console.log(10 * '5');
+console.log(10 / '5');
+console.log(10 % 'Javascript'); // NaN - 피연산자 숫자 변환 불가로 연산 수행 불가.
+
+// 비교 연산자로 크기를 비교하기 위해 숫자이어야 하기 때문에
+// 숫자가 아닌 피연산자로 숫자 타입으로 암묵적 타입 변환한다.
+console.log(10 > '5');
+
+// + 단항 연산자는 피연산자가 숫자 타입의 값이 아니면 숫자 타입의 값으로
+// 암묵적 타입 변환 수행
+console.log(+''); // 0 
+console.log(+'1'); // 1
+console.log(+'JavaScript'); // NaN
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(+null); // 0
+console.log(+undefined); // NaN
+console.log(+{}); // NaN
+console.log(+[]); // 0
+console.log(+function(){}); // NaN
+console.log(+Symbol()); // 
+
+
+
+
+
+
+
+
+
+
+
+
